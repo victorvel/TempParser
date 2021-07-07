48f9a1e6bbdc7b29f46eae83977bba250515c1e5
Eric Stubben <estubben2@gmail.com>
2021-07-07 16:36:28 -0600

    Updates to parser

--- Parser.py
+++ Parser.py
@@ -1,3 +1,4 @@
+import re
 
 class Parser:
 	def __init__(self, file_dict):
@@ -39,7 +40,17 @@ class Parser:
 				if l.find("_") == 0:
 					keys.append(l)
 				else:
-					l = l.split()
+					if '[' in l:
+    						l = l.split(' ',1)
+    						l[1] = l[1].strip('[]')
+    						temp = l[1].split()
+    						l[1] = ''
+    						for i, char in enumerate(temp):
+        						l[1] = l[1] + char
+        						if i < len(temp) - 1:
+            							l[1] = l[1] + ','
+					else:
+						l = l.split()
 					values[counter] = l 
 					counter += 1
 			self.insert_loop(keys, values)
@@ -48,10 +59,10 @@ class Parser:
 		for i in values:
 			for k in range(len(keys)):
 				if keys[k] in self.file_dict:
-					self.file_dict[keys[k]].append(values[i][k])
+					self.file_dict[keys[k]].append(self.to_numeric(values[i][k]))
 					continue
 		
-				self.file_dict[keys[k]] = [values[i][k]]
+				self.file_dict[keys[k]] = [self.to_numeric(values[i][k])]
 
 	def insert_dictionary(self, line):
 		if line[0] in self.file_dict:
@@ -62,12 +73,21 @@ class Parser:
 
 	def to_string(self, line):
 		str_1 = ""
+		if len(line) > 1:
+			if line[1][0] == '"':
+				line[1] = line[1].strip('"')
+				line[len(line)-1] = line[len(line)-1].strip('"')
+			elif line[1][0] == "'":
+				line[1] = line[1].strip("'")
+				line[len(line)-1] = line[len(line)-1].strip("'")
 		for i in range (1, len(line)):
 			str_1 += line[i] + " "
 				
 		return str_1.strip()
 
 	def to_numeric(self, x):
+		if re.search(r'\(\d+\)$',x):
+			x = x[:x.index('(')]
 		if x.isdigit() == True or self.check_float(x) == True:
 			return float(x) 
 		return x
